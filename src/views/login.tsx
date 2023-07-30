8b697e0d16f1e3c45d4563150e6ea03ebd348c01
danielrdzc <daniel.rdz.ecomm@gmail.com>
2023-07-29 21:36:35 -0600

    Added tailwind

--- src/views/login.tsx
+++ src/views/login.tsx
@@ -1,10 +1,26 @@
 import React from "react"
 export const Login = () => {
     return (
-        <div className="w-full flex items-center justify-center">
-            <div className="p-4">
-                <div>
+        <div className="w-full h-screen flex items-center justify-center">
+            <div className="bg-white rounded shadow-lg max-w-md">
+                <div className="mb-4">
                     <img src="http://happiiiercommunity.com/Images/Logo%20500x137%20transparente.png" alt="" />
+                </div>
+                <div className="">
+                    <form action="" className="flex flex-col">
+                        <div className="flex flex-col">
+                            <label htmlFor="">Usuario</label>
+                            <input className="border" type="text" />
+                        </div>
+                        <div className="flex flex-col">
+                            <label htmlFor="">Contraseña</label>
+                            <input className="border" type="password" />
+                        </div>
+
+                        <button>Ingresar</button>
+                        <a href="">¿Olvidaste la contraseña?</a>
+                        <div>¿Te interesa unirte? <a href="">Conoce más</a></div>
+                    </form>
                 </div>
             </div>
         </div>
